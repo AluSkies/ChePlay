c8fe349f1ab1043e4781874249fba6a59a942249
Gustavo Moura <mouralago@live.com>
2025-10-27 10:39:30 -0300

    Implementing Neo4J using Aura Cloud and fixing pom file.

---
+++ src/main/java/com/cheplay/config/Neo4jConfig.java
@@ -0,0 +1,8 @@
+package com.cheplay.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class Neo4jConfig { /* Config por defecto con Spring Boot */ }
